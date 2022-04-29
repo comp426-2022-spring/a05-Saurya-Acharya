156322ae9195835145912fbe639e761e48dae4aa
sauryaacharya <saurya.acharya9@gmail.com>
2022-04-29 15:21:20 -0400

    space

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.use( (req, res, next) => {
       secure: req.secure,
       status: res.statusCode,
       referer: req.headers['referer'],
-      useragent: req.headers['user-agent']
+      useragent: req.headers['user-agent'] 
     }
   const stmt = db.prepare(`INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url,  protocol, httpversion, secure, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
   const info = stmt.run(logdata.remoteaddr.toString(), logdata.remoteuser, logdata.time, logdata.method.toString(), logdata.url.toString(), logdata.protocol.toString(), logdata.httpversion.toString(), logdata.secure.toString(), logdata.status.toString(), logdata.referer, logdata.useragent.toString())
